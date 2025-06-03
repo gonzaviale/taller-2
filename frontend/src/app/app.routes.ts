389f035157d7d90bd99655dec58876399a7214ee
MM <143347704+MacielMaximiliano@users.noreply.github.com>
2025-06-03 19:11:21 -0300

    hola mundo

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+
+     {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+    },
     {
         path: 'login',
         loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
@@ -15,6 +20,6 @@ export const routes: Routes = [
     },
     {
         path: '**',
-        redirectTo: ''
+        redirectTo: 'home'
     }
 ];
