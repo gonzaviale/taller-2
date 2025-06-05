9487c262b8419a515ca7f2a8151906638cb3c0fe
gonzaviale <gonzaloleonelviale@gmail.com>
2025-06-04 22:24:15 -0300

    feat: :sparkles: add MenuComponent with navigation links and integrate into HomeComponent

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "../../layouts/menu/menu.component";
+
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
