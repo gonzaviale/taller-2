16f554a609c200a0c209594e3f97fa1d59c73785
gonzaviale <gonzaloleonelviale@gmail.com>
2025-06-22 20:08:40 -0300

    feat: :sparkles: add purchase handling in CartsService and update CartDTO types

--- frontend/src/app/services/carts/carts.service.ts
+++ frontend/src/app/services/carts/carts.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
-import { CartDTO, CartResponse } from '../../../types/CartDTO';
+import { CartDTO, PurchaseDTO, PurchaseRequest, PurchaseResponse } from '../../../types/CartDTO';
 import { Observable } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,25 +13,63 @@ export class CartsService {
   constructor() { }
 
   http = inject(HttpClient);
+  authService = inject(AuthService);
   apiUrl = environment.api_url;
 
-  getCarts(): Observable<CartResponse> {
-    return this.http.get<CartResponse>(`${this.apiUrl}/cart`);
+  // crear un carrito singleton para que no se pierda al refrescar la pagina
+  private cart: CartDTO = {
+    products: [],
+    totalPrice: 0
   }
 
-  getCartById(id: number): Observable<CartDTO> {
-    return this.http.get<CartDTO>(`${this.apiUrl}/cart/${id}`);
+  getCart(): CartDTO {
+    return this.cart;
   }
 
-  createCart(cart: CartDTO): Observable<CartDTO> {
-    return this.http.post<CartDTO>(`${this.apiUrl}/cart`, cart);
+  addToCart(product: CartDTO['products'][number]): void {
+    const existingProduct = this.cart.products.find(p => p.id === product.id);
+    if (existingProduct) {
+      existingProduct.quantity += product.quantity;
+    } else {
+      this.cart.products.push(product);
+    }
+    this.updateTotalPrice();
   }
 
-  updateCart(id: number, cart: CartDTO): Observable<CartDTO> {
-    return this.http.put<CartDTO>(`${this.apiUrl}/cart/${id}`, cart);
+  removeFromCart(productId: number): void {
+    this.cart.products = this.cart.products.filter(p => p.id !== productId);
+    this.updateTotalPrice();
   }
 
-  deleteCart(id: number): Observable<CartDTO> {
-    return this.http.delete<CartDTO>(`${this.apiUrl}/cart/${id}`);
+  private updateTotalPrice(): void {
+    this.cart.totalPrice = this.cart.products.reduce((total, product) => {
+      return total + (product.price * product.quantity);
+    }, 0);
+  }
+  
+  clearCart(): void {
+    this.cart.products = [];
+    this.cart.totalPrice = 0;
+  }
+
+  createPurchase(purchase: PurchaseRequest): Observable<PurchaseDTO> {
+    const userId = Number(this.authService.getUserId());
+    if (!userId) {
+      throw new Error('User ID is required to create a purchase');
+    }
+    const token = this.authService.getToken();
+    if (!token) {
+      throw new Error('Authentication token is required to create a purchase');
+    }
+    purchase.userId = userId;
+    return this.http.post<PurchaseDTO>(`${this.apiUrl}/purchase`, purchase);
+  }
+
+  updatePurchase(id: number, purchase: PurchaseDTO): Observable<PurchaseDTO> {
+    return this.http.put<PurchaseDTO>(`${this.apiUrl}/purchase/${id}`, purchase);
+  }
+
+  deleteCart(id: number): Observable<PurchaseDTO> {
+    return this.http.delete<PurchaseDTO>(`${this.apiUrl}/purchase/${id}`);
   }
 }
